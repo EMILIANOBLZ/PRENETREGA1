79f0286e2942b4e27ee893c31250a31f477e1054
emiliano blazquez <cokyblazquez8@gmail.com>
2023-07-19 19:12:10 -0300

    Javascript

--- apps.js
+++ apps.js
@@ -1,9 +1,9 @@
-alert("Hola estimado, bievenido a AMG DYNASTY, aca podras comprar tu lujoso Mercedes, Porsche y Audi con dolares americanos.")
+alert("Hola estimado, bievenido a AMG DYNASTY, aca podras comprar tu lujoso Mercedes, Porsche, Audi o Bmw con dolares americanos.")
 
 let salir
 
 while (salir !== "Salir") {
-    const ingresa = prompt("¿Qué desea comprar? Mercedes, Porsche o Audi?");
+    const ingresa = prompt("¿Qué vehiculo desea poseer? Mercedes, Porsche, Audi o Bmw?");
 
 
     if (ingresa === "Mercedes") {
@@ -17,9 +17,8 @@ while (salir !== "Salir") {
             const precio = prompt("Escriba Confirmar para confirmar la compra, y escriba Cancelar para cancelar la compra");
 
             if (precio === "Confirmar" || precio === "confirmar") {
-                alert("Transacción finalizada, usted ha recibido " + dolares + " Mercedes en la cuenta de su banco. ¡Muchas gracias por usar nuestro servicio!");
-                console.log(mercedes, "Mercedes");
-                alert
+                alert("Transacción finalizada, usted ha recibido " + dolares + "Mercedes a su propiedad. ¡Muchas gracias por usar nuestro servicio!");
+                console.log(dolares, "Mercedes");
             } else if (precio === "Cancelar" || precio === "cancelar") {
                 alert("Se canceló la compra. ¡Que tenga buenas tardes!");
             }
@@ -30,16 +29,16 @@ while (salir !== "Salir") {
 
     if (ingresa === "Porsche") {
         function Porsche() {
-            const dolares = parseInt(prompt("Ingrese la cantidad de Mercedes que desea comprar"));
+            const dolares = parseInt(prompt("Ingrese la cantidad de Porsche que desea comprar"));
 
-            const resultado = parseFloat(dolares * 395000);
+            const resultado = parseFloat(dolares * 1220000);
 
             console.log("El precio total es", resultado, "dólares americanos");
 
             const precio = prompt("Escriba Confirmar para confirmar la compra, y escriba Cancelar para cancelar la compra");
 
             if (precio === "Confirmar" || precio === "confirmar") {
-                alert("Transacción finalizada, usted ha recibido " + dolares + " Porsche en la cuenta de su banco. ¡Muchas gracias por usar nuestro servicio!");
+                alert("Transacción finalizada, usted ha recibido " + dolares + " Porsche a su propiedad. ¡Muchas gracias por usar nuestro servicio!");
                 console.log(dolares, "Porsche");
             } else if (precio === "Cancelar" || precio === "cancelar") {
                 alert("Se canceló la compra. ¡Que tenga buenas tardes!");
@@ -53,15 +52,15 @@ while (salir !== "Salir") {
         function Audi() {
             const dolares = parseInt(prompt("Ingrese la cantidad de Audi que desea comprar"));
 
-            const resultado = parseFloat(dolares * 98000);
+            const resultado = parseFloat(dolares * 795000);
 
             console.log("El precio total es", resultado, "dólares americanos");
 
             const precio = prompt("Escriba Confirmar para confirmar la compra, y escriba Cancelar para cancelar la compra");
 
             if (precio === "Confirmar" || precio === "confirmar") {
-                alert("Transacción finalizada, usted ha recibido " + dolares + " Audi en la cuenta de su banco. ¡Muchas gracias por usar nuestro servicio!");
-                console.log(dolares, "Mercedes");
+                alert("Transacción finalizada, usted ha recibido " + dolares + " Audi a de su propiedad. ¡Muchas gracias por usar nuestro servicio!");
+                console.log(dolares, "Audi");
             } else if (precio === "Cancelar" || precio === "cancelar") {
                 alert("Se canceló la compra. ¡Que tenga buenas tardes!");
             }
@@ -70,8 +69,29 @@ while (salir !== "Salir") {
         Audi();
     }
 
+    if (ingresa === "Bmw") {
+        function Bmw() {
+            const dolares = parseInt(prompt("Ingrese la cantidad de Bmw que desea comprar"));
 
-    salir = prompt("ingresa salir si quieres salir del programa, sino oprima cualquier tecla")
+            const resultado = parseFloat(dolares * 135000);
+
+            console.log("El precio total es", resultado, "dólares americanos");
+
+            const precio = prompt("Escriba Confirmar para confirmar la compra, y escriba Cancelar para cancelar la compra");
+
+            if (precio === "Confirmar" || precio === "confirmar") {
+                alert("Transacción finalizada, usted ha recibido " + dolares + " Bmw a su propiedad. ¡Muchas gracias por usar nuestro servicio!");
+                console.log(dolares, "Bmw");
+            } else if (precio === "Cancelar" || precio === "cancelar") {
+                alert("Se canceló la compra. ¡Que tenga buenas tardes!");
+            }
+        }
+
+        Bmw();
+    }
+
+
+    salir = prompt("ingresa salir si quieres salir de esta red, sino oprima cualquier tecla")
 
 
 
